8b4f8b3273774c40afb89e1fcac694f6f5343539
Stas <smaster950@gmail.com>
2022-10-13 20:10:06 +0300

    Complete Exercise29 with Method

--- Exercise29/Program.cs
+++ Exercise29/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите кол-во элементов массива: ");
+int length = int.Parse(Console.ReadLine() ?? "");
+
+int[] array = new int[length];
+
+FillArray(array);
+PrintArray(array);
+
+static void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write("Введите число для массива: ");
+        array[index] = int.Parse(Console.ReadLine() ?? "");
+        index++;
+    }
+}
+
+static void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int position = 0;
+    while (position < length)
+    {
+        Console.Write($"{array[position]} ");
+        position++;
+    }
+}
