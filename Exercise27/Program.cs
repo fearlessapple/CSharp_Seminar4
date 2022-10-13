edd333e894d85f72eeb33a285631568d87773882
Stas <smaster950@gmail.com>
2022-10-13 18:35:18 +0300

    Complete Exercise27 with Method

--- Exercise27/Program.cs
+++ Exercise27/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+SumCalculate(number);
+
+static void SumCalculate(int number)
+{
+    int number1 = number;
+    int sum = 0;
+    while (number > 0)
+    {
+        int LastDigit = number % 10;
+        int LastNumber = number / 10;
+        number = LastNumber;
+        sum = sum + LastDigit;
+    }
+    Console.Write($"{number1} -> {sum}");
+}
