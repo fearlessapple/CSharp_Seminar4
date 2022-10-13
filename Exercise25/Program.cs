b0f8ed4424723b2f2d4de935bddfc8ad3fe576f0
Stas <smaster950@gmail.com>
2022-10-13 16:20:07 +0300

    Complete Exercise25

--- Exercise25/Program.cs
+++ Exercise25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+
+int sum = 1;
+
+for (int i = 1; i <= numberB; i++)
+{
+    sum =  numberA * sum;
+}
+Console.WriteLine($"{numberA}, {numberB} -> {sum}");
